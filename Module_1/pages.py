edb17f7b16b46e93efee42397c2f5c83592800ce
Rob Boland <rob.n.boland@protonmail.com>
2025-05-19 21:22:42 -0700

    Baseline functionality restored

--- Module_1/pages.py
+++ Module_1/pages.py
@@ -6,6 +6,10 @@ bp = Blueprint("pages", __name__)
 def home():
     return render_template("pages/home.html")
 
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+@bp.route("/contact")
+def contact():
+    return render_template("pages/contact.html")
+
+@bp.route("/projects")
+def projects():
+    return render_template("pages/projects.html")
