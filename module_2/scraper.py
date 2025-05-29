20d55d701ea9dcb5f9b050b24d1879f0bca954b3
Rob Boland <rob.n.boland@protonmail.com>
2025-05-28 21:13:50 -0700

    Reading scratch work

--- module_2/scraper.py
+++ module_2/scraper.py
@@ -1,11 +1,25 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import mechanicalsoup
 
-url = "http://olympus.realpython.org/profiles/dionysus"
 
-page = urlopen(url)
-html = page.read().decode("utf-8")
+# 1
+browser = mechanicalsoup.Browser()
+url = "http://olympus.realpython.org/login"
+login_page = browser.get(url)
+login_html = login_page.soup
 
-soup = BeautifulSoup(html, "html.parser")
+# 2
+form = login_html.select("form")[0]
+form.select("input")[0]["value"] = "zeus"
+form.select("input")[1]["value"] = "ThunderDude"
 
-print(soup.title.sourceline)
+# 3
+profiles_page = browser.submit(form, login_page.url)
+
+links = profiles_page.soup.select("a")
+
+for link in links:
+    address = link["href"]
+    text = link.text
+    print(f"{text}: {address}")
