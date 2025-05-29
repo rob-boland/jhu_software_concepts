77a517252d251730a4dede45088b315dfba03c4f
Rob Boland <rob.n.boland@protonmail.com>
2025-05-28 22:46:57 -0700

    Building out parsing functionality

--- module_2/scraper.py
+++ module_2/scraper.py
@@ -1,7 +1,10 @@
 from urllib import parse, robotparser
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 agent = "rob"
 url = "https://www.thegradcafe.com/"
+survey_url = f"{url}survey/"
 
 def get_robots_txt(url:str, paths:list[str]) -> dict[str, bool]:
     """Fetches the robots.txt file for the given URL and checks if the provided
@@ -18,13 +21,32 @@ def get_robots_txt(url:str, paths:list[str]) -> dict[str, bool]:
 
     return allowed_paths
 
+paths = ["/", "/survey/"]
 
+survey_page = urlopen(survey_url)
+html = survey_page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
 
-paths = [
-        "/",
-        "/cgi-bin/",
-        "/admin/",
-        "survey/?program=Computer+Science"
-    ]
+def parse_column_titles(soup:BeautifulSoup) -> list[str]:
+    """Extract column titles from the survey page."""
+    column_titles = []
 
-print(get_robots_txt(url, paths))
+    # thead contains tr which contains th elements with column titles
+    thead = soup.find("thead")
+    tr = thead.find("tr")
+    if tr:
+        for th in tr.find_all("th"):
+            col_title_raw = th.get_text(strip=True)
+            col_title_formatted = col_title_raw.replace(" ", "_").lower()
+            if "sort" not in col_title_formatted:
+                column_titles.append(col_title_formatted)
+    
+    return column_titles
+
+tbody = soup.find("tbody")
+tr = tbody.find_all("tr")
+for row in tr:
+    if "class" not in row.attrs.keys():  # First line of result
+        for col in row.find_all("td")[:4]:
+            print(col.text.strip())
+        print("-" * 20)
