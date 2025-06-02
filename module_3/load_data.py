e964e9f12fc11f451d042a670578a7c64fd61636
Rob Boland <rob.n.boland@protonmail.com>
2025-06-01 23:39:15 -0700

    load_data.py initial work

--- module_3/load_data.py
+++ module_3/load_data.py
@@ -1,7 +1,8 @@
+import json
 import psycopg2
 from psycopg2 import OperationalError
 
-def create_connection(db_name, db_user, db_password, db_host, db_port):
+def create_connection(db_name:str, db_user:str, db_password:str, db_host:str="localhost", db_port:int=5432) -> psycopg2.extensions.connection:
     """Create a database connection to the PostgreSQL database specified by the
     connection parameters.
     
@@ -29,23 +30,58 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
         print(f"Error connecting to the database: {e}")
     return connection
 
-conn = create_connection(
-    db_name="grad_cafe",
-    db_user="rob",
-    db_password="C6pk88A#T5pt8nc$",
-    db_host="localhost",
-    db_port=5432
-)
-
-def close_connection(connection):
-    """Close the database connection.
+def insert_applicant_record(conn:psycopg2.extensions.connection, applicant_data:dict, applicant_i:int=None) -> None:
+    """Insert a new applicant record into the database.
     
     Args:
-        connection: A psycopg2 connection object.
+        conn (psycopg2.extensions.connection): Database connection object.
+        applicant_data (dict): Dictionary containing applicant data.
     """
-    if connection:
-        connection.close()
-        print("Database connection closed.")
-    else:
-        print("No connection to close.")
-close_connection(conn)
+    cursor = conn.cursor()
+    insert_query = """
+    INSERT INTO applicants (program, comments, date_added, url, status, term, us_or_international, gpa, gre, gre_v, gre_aw, degree)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """
+
+    try:
+        cursor.execute(insert_query, (
+            applicant_data['program_name'],
+            applicant_data['comments'],
+            applicant_data['date_of_information_added'],
+            applicant_data['url_link'],
+            " on ".join(applicant_data['applicant_status']),
+            applicant_data['program_start_semester'],
+            applicant_data['nationality'],
+            applicant_data['gpa'],
+            applicant_data['gre_score'],
+            applicant_data['gre_v_score'],
+            applicant_data['gre_aw_score'],
+            applicant_data['program_level']
+        ))
+        conn.commit()
+    except Exception as e:
+        print(f"Error inserting record: {e} on applicant number {applicant_i}")
+        conn.rollback()
+    finally:
+        cursor.close()
+
+
+if __name__ == "__main__":
+    applicant_data_path = r"module_2\applicant_data.json"
+    with open(applicant_data_path, 'r', encoding='utf-8') as file:
+        applicant_data = json.load(file)
+    # Create a connection to the PostgreSQL database
+    conn = create_connection(
+        db_name="grad_cafe",
+        db_user="rob",
+        db_password="C6pk88A#T5pt8nc$",
+        db_host="localhost",
+        db_port=5432
+    )
+
+    for applicant_i, applicant in enumerate(applicant_data):
+        # Insert each applicant record into the database
+        print(f"Inserting applicant {applicant_i} of {len(applicant_data)}")
+        insert_applicant_record(conn, applicant, applicant_i)
+
+    conn.close()
