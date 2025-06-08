3bb6b0330a99cdc2e81a26e613bb2b9ebe5aaec6
Rob Boland <rob.n.boland@protonmail.com>
2025-06-07 22:20:35 -0700

    Continued Flask database integrations

--- module_3/sql_presentation/__init__.py
+++ module_3/sql_presentation/__init__.py
@@ -1,25 +1,8 @@
 from flask import Flask
-import psycopg2
-import json
 
 # Module imports
 from sql_presentation import pages
-from load_data import create_connection
 
-def get_db_connection(db_config_path:str) -> psycopg2.extensions.connection:
-    with open(db_config_path, 'r', encoding='utf-8') as file:
-            db_config = json.load(file)
-
-    # Create a connection to the PostgreSQL database
-    conn = create_connection(
-        db_name=db_config["db_name"],
-        db_user=db_config["db_user"],
-        db_password=db_config["db_password"],
-        db_host=db_config["db_host"],
-        db_port=db_config["db_port"]
-    )
-    
-    return conn
 
 def create_app():
     """Create and configure the Flask application."""
--- module_3/sql_presentation/pages.py
+++ module_3/sql_presentation/pages.py
@@ -1,8 +1,35 @@
 from flask import Blueprint, render_template
 
+import psycopg2
+import json
+
+from load_data import create_connection
+import query_data
+
 bp = Blueprint("pages", __name__)
+
+def get_db_connection() -> psycopg2.extensions.connection:
+    with open(r"D:\GitHub\JHU 25-01\jhu_software_concepts\module_3\data\db_config.json", 'r', encoding='utf-8') as file:
+            db_config = json.load(file)
+
+    # Create a connection to the PostgreSQL database
+    conn = create_connection(
+        db_name=db_config["db_name"],
+        db_user=db_config["db_user"],
+        db_password=db_config["db_password"],
+        db_host=db_config["db_host"],
+        db_port=db_config["db_port"]
+    )
+    
+    return conn
 
 # Define routes for each page
 @bp.route("/")
 def home():
+
+    conn = get_db_connection()
+
+    test = query_data.count_semester_entries(conn, "Fall 2024")
+    print(test)
+
     return render_template("pages/home.html")
