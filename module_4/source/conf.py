0dd8dd45fb3f63e074c713bdc3dd2b5331fbb133
Rob Boland <rob.n.boland@protonmail.com>
2025-06-13 13:20:16 -0700

    Update conf.py

--- module_4/source/conf.py
+++ module_4/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'module_4'
 copyright = '2025, Jon Robert Boland'
 author = 'Jon Robert Boland'
@@ -14,7 +19,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
